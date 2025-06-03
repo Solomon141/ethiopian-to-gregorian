e90dadb909086295068e03b7d2b4490199903056
solomon41411 <weymit2001@gmail.com>
2025-06-03 09:42:59 +0300

    update package versions and main entry point; remove old index file

---
+++ index.js
@@ -0,0 +1,15 @@
+import getEthiopicDate from "./src/gregoriantoethiopian.js";
+import {
+  etchangetofull,
+  etchangetolong,
+  etchangetomedium,
+  etchangetoshort,
+} from "./src/et2gre/dateformats.js";
+
+export {
+  getEthiopicDate,
+  etchangetofull,
+  etchangetolong,
+  etchangetomedium,
+  etchangetoshort,
+};
